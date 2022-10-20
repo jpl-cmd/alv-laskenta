a92b3b97c455c921fed21ccacc02716596517fc5
unknown <jpetteriliukkonen@gmail.com>
2022-10-20 21:44:04 +0300

    Lasku-luokka

---
+++ src/main/java/data/Lasku.java
@@ -0,0 +1,46 @@
+package data;
+
+import java.util.ArrayList;
+
+public class Lasku {
+
+	private ArrayList<Tuote> tuotteet;
+	private double summa;
+	
+	public Lasku() {
+		this.tuotteet = new ArrayList<Tuote>();
+		this.summa = 0.0;
+	}
+	
+	public void lisaaTuote(Tuote tuote) {
+		this.tuotteet.add(tuote);
+	}
+
+	public ArrayList<Tuote> getTuotteet() {
+		return tuotteet;
+	}
+
+	public void setTuotteet(ArrayList<Tuote> tuotteet) {
+		this.tuotteet = tuotteet;
+	}
+
+	public double getSumma() {
+		return summa;
+	}
+
+	public void setSumma(double summa) {
+		this.summa = summa;
+	}
+	
+	public void laskeSumma() {
+		double summa = 0;
+		for(Tuote tuote : tuotteet) {
+			summa += tuote.getHinta();
+		}
+		
+		this.summa = summa;
+	}
+	
+	
+	
+}
