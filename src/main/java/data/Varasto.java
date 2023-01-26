fd44ddc722ac917055da85186376764f1478e846
unknown <jpetteriliukkonen@gmail.com>
2023-01-26 22:31:09 +0200

    Lisätty puuttuvat tiedostot

---
+++ src/main/java/data/Varasto.java
@@ -0,0 +1,9 @@
+package data;
+
+public class Varasto {
+	
+	public static int TELEVISIO = 100;
+	public static int MAITO = 100;
+	public static int KIRJA = 100;
+	public static int JAKKARA = 100;
+}
